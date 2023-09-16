a0e1745c8c66f44bc0dccc9189ef66561ea1755c
Kevin Lee <kevinlfcola@gmail.com>
2023-09-15 21:46:19 -0700

    GET /urls/new done

--- routes/urls.js
+++ routes/urls.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const urlsRouter = express.Router();
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
+urlsRouter.use(cookieParser());
 
 urlsRouter.get("/new", (req, res) => {
-    res.render("newUrl");
+    if (req.cookies.user) {
+        res.render("newUrl");
+    } else {
+        res.redirect("/auth/login");
+    }
 });
 
 module.exports = urlsRouter;
